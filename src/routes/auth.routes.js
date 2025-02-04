f9fb5c86f609ea922ee9242618c470f4862c045c
Viraj <vis68354@gmail.com>
2025-02-04 19:07:31 +0530

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { sendOTP, verifyOTP, logout, checkLoggedIn } from '../controllers/auth.controller.js';
+import { sendOTP, verifyOTP, logout, checkLoggedIn, sendOTPTest } from '../controllers/auth.controller.js';
 import { authenticate } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/',checkLoggedIn);
 router.post('/send-otp', sendOTP);
+router.post('/send-otp-test', sendOTPTest);
 router.post('/verify-otp', verifyOTP);
 router.post('/logout',authenticate, logout);
 
